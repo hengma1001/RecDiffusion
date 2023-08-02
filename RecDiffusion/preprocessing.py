29e5c8450417bafcf2167771b5ef3ce8f99bf113
Ma, Heng  <maheng1991@gmail.com>
2023-08-02 10:10:57 -0500

    add onehot encoding to prepro

--- RecDiffusion/preprocessing.py
+++ RecDiffusion/preprocessing.py
@@ -3,10 +3,15 @@ import MDAnalysis as mda
 def pdb_to_dict(pdb_file, input_feature=None, node_attribute=None):
     data = {}
     mda_u = mda.Universe(pdb_file)
+    
     data['pos'] = mda_u.atoms.positions
     if input_feature:
         assert len(input_feature) == mda_u.atoms.n_atoms
         data['x'] = input_feature
     if node_attribute:
-        data['y'] = mda_u.atoms.names
+        data['y'] = [attr_to_onehot(resname + atomname) for atomname, resname in zip(mda_u.atoms.names, mda_u.atom.resnames)]
     return data
+
+
+def attr_to_onehot(res_atom_name):
+    pass
